c07da870ae90b88fe6d9e64e2cc6e8f7ed027e10
Varun Sundar <varun19299@gmail.com>
2018-09-21 11:48:10 +0530

    assign3$

--- assignment-3/ee16b068_1.cu
+++ assignment-3/ee16b068_1.cu
@@ -6,124 +6,98 @@ bool checkWord(word){
     // Check if word meets, else pre-process
 }
 
-__global__ void MatrixMulKernel_col_maj(double* M, double* N, double* P, int Width) { 
-    // Calculate the row index of the P element and M
-    int Row = blockIdx.y*blockDim.y+threadIdx.y;
-    // Calculate the column index of P and N
-    int Col = blockIdx.x*blockDim.x+threadIdx.x; 
-    
-    if ((Row < Width) && (Col < Width)) {
-            float Pvalue = 0;
-        for (int k = 0; k < Width; ++k) {
-            Pvalue += M[Row*Width+k]*N[k*Width+Col];
-        }
-            P[Row*Width+Col] = Pvalue;
-        }
-    }
+__global__ void windowGram(char** word){
 
-__global__ void MatrixMulKernel_row_maj(double* M, double* N, double* P, int Width) { 
-    // Calculate the row index of the P element and M
-    int Row = blockIdx.y*blockDim.y+threadIdx.x;
-    // Calculate the column index of P and N
-    int Col = blockIdx.x*blockDim.x+threadIdx.y; 
-    
-    if ((Row < Width) && (Col < Width)) {
-            float Pvalue = 0;
-        for (int k = 0; k < Width; ++k) {
-            Pvalue += M[Row*Width+k]*N[k*Width+Col];
-        }
-            P[Row*Width+Col] = Pvalue;
-        }
-    }
+}
 
 int main(int argc,char **argv) {
-    int N = 8192;
-    size_t size = N *N* sizeof(double);
+    
+    //size_t size = N *N* sizeof(double);
 
-    double*h_matA = (double*)malloc(size);
-    double*h_matB = (double*)malloc(size);
-    double*h_matC = (double*)malloc(size); // result
-
-    int loop1; int loop2; // loop variables
-    float time_spent;
-
-    fill_matrix(h_matA,N,N);
-    fill_matrix(h_matB,N,N);
-
-    printf("\nMatrix A (first 10*10 inputs)\n");
-    for(loop1 = 0; loop1 < 10; loop1++){
-        for (loop2=0;loop2 < 10; loop2++)
-            printf("%f ", *(h_matA + N*loop1 + loop2));
+    int N = atoi(argv[1]);
+    char *filename = argv[2];
+    char words[MAXWORDS * 20];
+    // For calculating N-count-grams
+    // Filename: shaks.txt
+    // Stores all words in 1D array
+    // Single word length is bounded by 20
+    // Take input string into this
+    char curWord[40];
+    int totalWordCount = 0;
+    FILE *ipf = fopen(filename, “r”);
+    while (fscanf(ipf, “%s ”, curWord) != EOF
+                    // Count of number of words read
+    && totalWordCount < MAXWORDS) {
+        //checkWord(curWord, ...);
+        printf("Current word %s \n",curWord);
+        totalWordCount += 1;
     }
+    fclose(ipf);
+    // Check for word properties
+    // and update ‘words[]’ array.
+    // Modify this section according
+    // to below mentioned properties
 
-    printf("\n\nMatrix B (first 10*10 inputs)\n");
-    for(loop1 = 0; loop1 < 10; loop1++){
-        for (loop2=0;loop2 < 10; loop2++)
-            printf("%f ", *(h_matB + N*loop1 + loop2));
-    }
+    // cudaMalloc(&d_matC, size);
 
-    double* d_matA;   cudaMalloc(&d_matA, size);
-    double* d_matB;   cudaMalloc(&d_matB, size);
-    double* d_matC;   cudaMalloc(&d_matC, size);
+    // //GPU timing
+    // cudaEvent_t start, stop;
+    // cudaEventCreate(&start);
+    // cudaEventCreate(&stop);
 
-    //GPU timing
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
+    // // Copy vectors from host memory to device memory
+    // cudaMemcpy(d_matA, h_matA, size,cudaMemcpyHostToDevice);
+    // cudaMemcpy(d_matB, h_matB, size,cudaMemcpyHostToDevice);
 
-    // Copy vectors from host memory to device memory
-    cudaMemcpy(d_matA, h_matA, size,cudaMemcpyHostToDevice);
-    cudaMemcpy(d_matB, h_matB, size,cudaMemcpyHostToDevice);
+    // // Invoke kernel
+    // dim3 threadsPerBlock = (16,16);
+    // dim3 blocksPerGrid ((N + threadsPerBlock.x - 1) /threadsPerBlock.x,(N + threadsPerBlock.y - 1) /threadsPerBlock.y);
 
-    // Invoke kernel
-    dim3 threadsPerBlock = (16,16);
-    dim3 blocksPerGrid ((N + threadsPerBlock.x - 1) /threadsPerBlock.x,(N + threadsPerBlock.y - 1) /threadsPerBlock.y);
+    // cudaEventRecord(start, 0);
+    // MatrixMulKernel_col_maj<<<blocksPerGrid, threadsPerBlock>>>(d_matA,d_matB, d_matC, N);
+    // cudaEventRecord(stop, 0);
+    // cudaEventSynchronize(stop);
+    // cudaEventElapsedTime(&time_spent, start, stop);
+    // printf("\nTime spent in col maj %f\n",time_spent);
 
-    cudaEventRecord(start, 0);
-    MatrixMulKernel_col_maj<<<blocksPerGrid, threadsPerBlock>>>(d_matA,d_matB, d_matC, N);
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&time_spent, start, stop);
-    printf("\nTime spent in col maj %f\n",time_spent);
+    // // h_C contains the result in host memory
+    // cudaMemcpy(h_matC, d_matC, size,cudaMemcpyDeviceToHost);
 
-    // h_C contains the result in host memory
-    cudaMemcpy(h_matC, d_matC, size,cudaMemcpyDeviceToHost);
+    // printf("\n\nMatrix C via col major (first 10*10 outputs)\n");
+    // for(loop1 = 0; loop1 < 10; loop1++){
+    //     for (loop2=0;loop2 < 10; loop2++)
+    //         printf("%f ", *(h_matC + N*loop1 + loop2));
+    // }
 
-    printf("\n\nMatrix C via col major (first 10*10 outputs)\n");
-    for(loop1 = 0; loop1 < 10; loop1++){
-        for (loop2=0;loop2 < 10; loop2++)
-            printf("%f ", *(h_matC + N*loop1 + loop2));
-    }
+    // cudaEventRecord(start, 0);
+    // MatrixMulKernel_row_maj<<<blocksPerGrid, threadsPerBlock>>>(d_matA,d_matB, d_matC, N);
+    // cudaEventRecord(stop, 0);
+    // cudaEventSynchronize(stop);
+    // cudaEventElapsedTime(&time_spent, start, stop);
+    // printf("\nTime spent in row maj %f\n",time_spent);
 
-    cudaEventRecord(start, 0);
-    MatrixMulKernel_row_maj<<<blocksPerGrid, threadsPerBlock>>>(d_matA,d_matB, d_matC, N);
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&time_spent, start, stop);
-    printf("\nTime spent in row maj %f\n",time_spent);
+    // // h_C contains the result in host memory
+    // cudaMemcpy(h_matC, d_matC, size,cudaMemcpyDeviceToHost);
 
-    // h_C contains the result in host memory
-    cudaMemcpy(h_matC, d_matC, size,cudaMemcpyDeviceToHost);
-
-    printf("\n\nMatrix C via row major (first 10*10 outputs)\n");
-    for(loop1 = 0; loop1 < 10; loop1++){
-        for (loop2=0;loop2 < 10; loop2++)
-            printf("%f ", *(h_matC + N*loop1 + loop2));
-    }
+    // printf("\n\nMatrix C via row major (first 10*10 outputs)\n");
+    // for(loop1 = 0; loop1 < 10; loop1++){
+    //     for (loop2=0;loop2 < 10; loop2++)
+    //         printf("%f ", *(h_matC + N*loop1 + loop2));
+    // }
       
 
-    // Log outputs
-    printf("\nWritting to file assignment_2_1_out as Mat C");
-    print_matrix_to_file(h_matC,N,N);
+    // // Log outputs
+    // printf("\nWritting to file assignment_2_1_out as Mat C");
+    // print_matrix_to_file(h_matC,N,N);
 
-    // Free device memory
-    cudaFree(d_matA);
-    cudaFree(d_matB);
-    cudaFree(d_matC);
+    // // Free device memory
+    // cudaFree(d_matA);
+    // cudaFree(d_matB);
+    // cudaFree(d_matC);
 
-    // Free host memory
-    free(h_matA);
-    free(h_matB);
-    free(h_matC);
+    // // Free host memory
+    // free(h_matA);
+    // free(h_matB);
+    // free(h_matC);
     return 0;
 }
